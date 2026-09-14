96f37e735a7036f4daf0f2b5eb93619a1abc2183
Catalin951 <manolecatalin117@gmail.com>
2026-09-14 14:24:52 +0300

    regenerate html when updating delivry option

--- data/cart.js
+++ data/cart.js
@@ -44,3 +44,15 @@ export function removeFromCart(productId) {
 
   saveToStorage();
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem = null;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  saveToStorage();
+}
