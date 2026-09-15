164e4afffb565ec5107a60c999ecba1f57d23fc7
Catalin951 <manolecatalin117@gmail.com>
2026-09-15 20:44:16 +0300

    used jasmine to test formatCurrency

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,25 @@
+import formatCurrency from "../scripts/utils/money.js";
+
+describe('test suite: formatCurrency', () => {
+    describe('test type: format odds', () => {
+        it('bigger than 0', () =>
+            expect(formatCurrency(1)).toEqual('0.01')
+        );
+        it('smaller than 0', () =>
+            expect(formatCurrency(-11)).toEqual('-0.11')
+        );
+    })
+
+    it('converts cents into dollars', () =>
+        expect(formatCurrency(2095)).toEqual('20.95')
+    );
+
+    it('works with 0', () =>
+        expect(formatCurrency(0)).toEqual('0.00')
+    );
+
+    it('rounds up to the nearest cent', () =>
+        expect(formatCurrency(2000.5)).toEqual('20.01')
+    );
+
+});
